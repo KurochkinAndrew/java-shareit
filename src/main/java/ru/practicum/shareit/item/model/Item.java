7d9dc2fba4dc571de98adc63e75bf8cf0f22ebc2
Курочкин Андрей <andrylol228@mail.ru>
2023-10-01 14:00:37 +0300

    Финальный проект 14 спринта итерация 1.1

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.request.ItemRequest;
 
 import javax.persistence.*;
 
